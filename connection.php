dabdc443d137aac8ce65bf7791978e941a9f106b
ptrslmn <iam.ekaokta@gmail.com>
2022-11-11 23:12:50 +0700

    first commit

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'paswa2';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+?>
